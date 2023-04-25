064ce575f0a4de112069a0c343205746ce1b5c8b
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-04-25 06:46:18 -0300

    Add addBockToList function

--- js/index.js
+++ js/index.js
@@ -5,6 +5,8 @@ const addBtn = document.getElementsByName('add')[0];
 const savedBookData = localStorage.getItem('books-data');
 const data = savedBookData ? JSON.parse(savedBookData) : [];
 
+
+
 const saveBookForm = () => {
   localStorage.setItem('books-data', JSON.stringify(data));
 };
@@ -16,4 +18,22 @@ addBtn.addEventListener('click', () => {
     author: authorInput.value,
   });
   saveBookForm();
+  addBookToList();
 });
+const booksContainer = document.querySelector('.display-book');
+
+function addBookToList() {
+  data.forEach(e => {
+  
+    booksContainer.innerHTML += `
+      <div class="book-item">
+        <p>${e.title}</p>
+        <p>${e.author}</p>
+        <button id="item${e.id}"type="button" class="btn-remove">Remove</button>
+      </div>
+      <hr>`
+    console.log(e)
+  });
+}
+
+addBookToList()
